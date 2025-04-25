e06de91cd7869a01842c344852aa85bf2848df6a
lkklkklbb <lkklkklbb@gmail.com>
2025-04-26 00:11:02 +0300

    Create home.tsx

---
+++ client/src/pages/home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to TechStudents Portal</h1>
+      <p>This is the home page.</p>
+    </div>
+  );
+}
